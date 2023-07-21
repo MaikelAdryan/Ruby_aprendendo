c35d332d951cdd45c7f0129e401e7f54bacfbe44
AdryanMaikel <adryanmaikel@gmail.com>
2023-07-21 16:35:44 -0300

    update

--- message.rb
+++ message.rb
@@ -3,7 +3,8 @@ class Message
       quem_informou, tabela,
       linha, horario, sentido,
       carro, tipo_ocor, 
-      local_ou_atraso, ocorrencia
+      local_ou_atraso, ocorrencia,
+      new_carro = ""
 
     @informado = informado
     @quem_informou = quem_informou
@@ -15,13 +16,15 @@ class Message
     @tipo_ocorrencia = tipo_ocor
     @local_ou_atraso = local_ou_atraso
     @ocorrencia = ocorrencia
+    @new_carro =  new_carro.to_s
     text_end = ""
-    new_carro = nil
+   
   end
 
-  def message_whatz
+  def whatz
     text_end = "*CCO INFORMA:*\n"
-    if @informado == true
+    case @informado
+    when true
       text_end = text_end + "Conforme informado ao CCO pelo "
     else 
       text_end = text_end + "Conforme contato feito pelo CCO ao "
@@ -39,48 +42,72 @@ class Message
       text_end = text_end + "fiscal " + @quem_informou +"\n"
     end
     
-    text_end = text_end + "- " + @tabela + "\n"#carro #{@carro}\n"
+    text_end = text_end + "- " + @tabela + " carro #{@carro}\n"#carro #{@carro}\n"
     text_end = text_end + "- #{@linha} das #{@horario} #{@sentido}\n"
 
     text_end = text_end + "- Viagem "
 
-    if @tipo_ocorrencia == "interrompida"
+    if @tipo_ocorrencia == "int"
       text_end = text_end + "interrompida na #{@local_ou_atraso}\n"
-    elsif @tipo_ocorrencia == "atraso"
+    elsif @tipo_ocorrencia == "atr"
       text_end = text_end + "realizada com #{@local_ou_atraso} minutos de atraso\n"
-    elsif @tipo_ocorrencia == "afrente"
-      text_end = text_end + "realizada a partir da #{@local_ou_atraso}\n"
+    elsif @tipo_ocorrencia == "afr"
+      text_end = text_end + "realizada a partir da "+@local_ou_atraso+"\n"
     end
 
     text_end = text_end + "- Motivo: devido a "
 
-    if @ocorrencia == "congestionamento"
+    # if @ocorrencia == "congestionamento"
+    #   text_end = text_end + "congestionamento, "
+    # elsif @ocorrencia == "pneu"
+    #   text_end = text_end + "pneu furado do carro #{carro}"
+    # else
+      
+    # end
+    case @ocorrencia
+    when "congestionamento"
       text_end = text_end + "congestionamento"
-    elsif @ocorrencia == "apd"
-      text_end = text_end + "problemas no apd do #{@carro}"
+    when "tempo"
+      text_end = text_end + "tempo insuficiente"
+    when "pneu"
+      text_end = text_end + "pneu furado do #{carro}"
+    when "apd"
+      text_end = text_end + "problemas no #{@ocorrencia} do carro #{@carro}"
+    when "motor"
+      text_end = text_end + "problemas no #{@ocorrencia} do carro #{@carro}"
+    when "suspensão"
+      text_end = text_end + "problemas na #{@ocorrencia} do carro #{@carro}"
+    when "embreagem"
+      text_end = text_end + "problemas na #{@ocorrencia} do carro #{@carro}"
+    when "mot"
+      text_end = text_end + "problemas com o motorista"
     else
-      text_end = text_end + ""
+        text_end = text_end + ""
     end
     
-    
-      
+    if @new_carro != "" then
+      text_end = text_end + ", trocado pelo carro #{@new_carro}"
+    else
+      text_end
+    end
   end
 end
 
 
-message = Message.new false, #informado
+message = Message.new true, #informado?
 3, #empresa ou fiscal "" -> nome fiscal
-"652/013", #tabela
-"654", #linha
-"14:20", #horario
+"659/012", #tabela
+"861", #linha
+"15:33", #horario
 "BC",#sentido
-6414,
-"atraso", #ocorrencia
-"8", #local ou tempo de atraso
-"apd" #ocorrencia
+"6418",#carro
+"atr", #ocorrencia
+"Av. Baltazar de Oliveira Garcia, após a Av. Sertório", #local ou tempo de atraso
+"mot" #ocorrencia
+ #trocado pelo?
 
 
-puts message.message_whatz
+puts message.whatz
 
 # CCO INFORMA:
 # Conforme contato feito pelo CCO ao largador Navegantes
